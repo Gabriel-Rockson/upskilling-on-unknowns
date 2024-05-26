3b14383e84de149a6fb04a35b30eb5f774a3141c
Gabriel Rockson <gabrielrockson99@gmail.com>
2024-05-26 05:44:44 +0000

    study basic types

---
+++ programming-languages/golang/go_tutorials/cmd/tutorial_1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intNum int
+	fmt.Println(intNum)
+
+	var floatNum float64
+	fmt.Println(floatNum)
+
+	var String string
+	fmt.Println(String)
+
+	var1, var2 := 1, 2
+	fmt.Println(var1, var2)
+
+	const myConst string = "const value"
+	fmt.Println(myConst)
+}
