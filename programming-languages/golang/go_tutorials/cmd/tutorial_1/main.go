b2c29e1665ceeb45613d471a0cdf61ac1edbf633
Gabriel Rockson <gabrielrockson99@gmail.com>
2024-05-26 12:30:46 +0000

    study maps and looping in golang

--- programming-languages/golang/go_tutorials/cmd/tutorial_1/main.go
+++ programming-languages/golang/go_tutorials/cmd/tutorial_1/main.go
@@ -3,39 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	intArray := [...]int{1, 2, 3}
-	fmt.Println(intArray)
+	// Maps in golang
+	firstMap := make(map[string]uint)
+	fmt.Printf("Value = %v, Length = %d\n", firstMap, len(firstMap))
+	firstMap["Gabriel"] = 24
+	fmt.Printf("Value = %v, Length = %d\n", firstMap, len(firstMap))
 
-	intSlice1 := []int{1, 2, 3}
-	fmt.Printf(
-		"Value = %v, Length = %d, Capacity = %d\n",
-		intSlice1,
-		len(intSlice1),
-		cap(intSlice1),
-	)
-	intSlice1 = append(intSlice1, 9)
-	fmt.Printf(
-		"Value = %v, Length = %d, Capacity = %d\n",
-		intSlice1,
-		len(intSlice1),
-		cap(intSlice1),
-	)
+	secondMap := map[string]uint{"Gabriel": 24, "Melinda": 22}
+	fmt.Printf("Value = %v\n", secondMap)
 
-	intSlice2 := []int{12, 13, 15}
+	age, ok := secondMap["Melindas"]
+	if !ok {
+		fmt.Printf("Name Melinda is not in the map\n")
+	} else {
+		fmt.Printf("Age of Melinda is %v\n", age)
+	}
 
-	intSlice1 = append(intSlice1, intSlice2...)
-	fmt.Printf(
-		"Value = %v, Length = %d, Capacity = %d\n",
-		intSlice1,
-		len(intSlice1),
-		cap(intSlice1),
-	)
+	// Looping
 
-	intSlice3 := make([]int, 0, 50)
-	fmt.Printf(
-		"Value = %v, Length = %d, Capacity = %d\n",
-		intSlice3,
-		len(intSlice3),
-		cap(intSlice3),
-	)
+	// looping over a map ( the order is not preserved when looping over maps )
+	for name, age := range secondMap {
+		fmt.Printf("Name = %v, Age = %d\n", name, age)
+	}
+
+	// looping over an array or slice
+	nameSlice := []string{"Gabriel", "Rockson", "Melinda", "Ampah", "Korsah"}
+	for i, v := range nameSlice {
+		fmt.Printf("Index = %d, Name = %v\n", i, v)
+	}
 }
